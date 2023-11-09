aaaa62907e5fac8b6b7ba6a044fe65e45795b0cf
YoonJunHyeon <qmdldos@gmail.com>
2023-11-09 11:42:08 +0900

    예외 처리 작성

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log('app.js loaded');
+
+// npm install mongodb -> MongoDB Node.js 드라이버를 설치한다.
+
+async function main(){
+  // atlas는 대시보드에서 연결 문자열을 사용할 수 있다.
+  // 연결 문자열과 같은 민감한 정보는 하드코딩 대신 환경 변수를 통해 관리하는 것이 좋다.
+  //process.env 객체를 사용하는 방법을 채택
+  const uri = process.env.MONGODB_URI; 
+  console.log(uri);
+  
+  const client = 
+    new MongoClient(
+      uri,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+
+  try {
+    
+  }catch(err) {
+    console.log(err, "catch 구분 작동");
+  } finally {
+    await client.close() // MongoDB 연결 닫기
+  }
+}
+
+main().catch(console.error);
