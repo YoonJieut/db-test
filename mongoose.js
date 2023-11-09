b1bc23f401c5a1c6b49492c57b01579750e8b5fb
YoonJunHyeon <qmdldos@gmail.com>
2023-11-09 17:04:57 +0900

    모든 게시글 조회

--- mongoose.js
+++ mongoose.js
@@ -24,12 +24,19 @@ const postSchema = new mongoose.Schema({
 // ? 모델이란 무엇인가?
 // * 데이터를 구조화하고 조작하는데 사용되는 개념! 
 // * 스키마를 기반으로 만들어진 객체
+
+/**
+ * 1번째 파라미터는 모델이름 = 컬렉션이름
+ * 2번째 파라미터는 스키마객체
+ */
 const Post = mongoose.model('Post', postSchema);
+
+// ----------------------------------------------
 
 // 새 게시글 생성 및 저장
 async function createPost() {
   const newPost = new Post({
-    title: 'My First',
+    title: 'My First 몽구쓰',
     body: 'This is the content.',
     tags: ['new', 'featured']
   });
@@ -37,4 +44,16 @@ async function createPost() {
   const result = await newPost.save();
   console.log('Post Created:', result);
 }
+
+// 모든 게시글 조회
+async function getPosts() {
+  const posts = await Post.find();
+  console.log('All Posts:', posts);
+}
+
+
+
+// ------------------------------ 실행함수 모음
+createPost();
+getPosts();
 
