721d1ce36b7791b83a24155dc09099253aab3f96
YoonJunHyeon <qmdldos@gmail.com>
2023-11-09 16:21:32 +0900

    try catch finally를 활용하여서 db 관련 로직 준비 완료

--- original.js
+++ original.js
@@ -3,3 +3,15 @@ console.log('original Start!');
 const uri = 'mongodb://localhost:27017/';
 const client = new MongoClient(uri);
 
+async function main() {
+  try{
+
+  }catch(err){
+    console.log("catch error block" + err);
+  } finally {
+    // MongoDb 닫기 -> 리로스 누출을 막아야 한다.
+    await client.close();
+  }
+
+
+}
