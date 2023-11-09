0f45baac89b6166ef0094a888419ccf75836d9e2
YoonJunHyeon <qmdldos@gmail.com>
2023-11-09 16:52:16 +0900

    done: mongoose.connect test done

--- mongoose.js
+++ mongoose.js
@@ -3,9 +3,9 @@ console.log('mongoose.js start!');
 const mongoose = require('mongoose');
 
 // ? userNewUrlParser 이런 건 최신 버전에서 사라졌다는데 지금도 필요할까?
+// ! 역시나 필요 없었다.
 
 mongoose.connect(
-  "mongodb://127.0.0.1:27017/blog", 
-  { useNewUrlParser: true, useUnifiedTopology: true }
+  "mongodb://127.0.0.1:27017/blog"
 )
 
