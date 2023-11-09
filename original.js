c918f5990ab8dbfc0cad82130ff55beda2a0918c
YoonJunHyeon <qmdldos@gmail.com>
2023-11-09 16:23:07 +0900

    db 연결

--- original.js
+++ original.js
@@ -5,6 +5,11 @@ const client = new MongoClient(uri);
 
 async function main() {
   try{
+    // connect로 연결해주기
+    await client.connect();
+    console.log('connected to db');
+
+    // * 이제 여기에 발동될 로직 추가.
 
   }catch(err){
     console.log("catch error block" + err);
