918c416496cb91fd77864102ae7f1e30fd525fa9
YoonJunHyeon <qmdldos@gmail.com>
2023-11-09 16:59:56 +0900

    ! 모델의 등장 그리고 new 생성자를 통해 인스턴스를 만든 뒤, .save로 저장한다.

--- mongoose.js
+++ mongoose.js
@@ -9,3 +9,32 @@ mongoose.connect(
   "mongodb://127.0.0.1:27017/blog"
 )
 
+// * 스키마 정의 : mongoose.Schema
+const postSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+  tags: [String],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// 모델 생성
+// ? 모델이란 무엇인가?
+// * 데이터를 구조화하고 조작하는데 사용되는 개념! 
+// * 스키마를 기반으로 만들어진 객체
+const Post = mongoose.model('Post', postSchema);
+
+// 새 게시글 생성 및 저장
+async function createPost() {
+  const newPost = new Post({
+    title: 'My First',
+    body: 'This is the content.',
+    tags: ['new', 'featured']
+  });
+
+  const result = await newPost.save();
+  console.log('Post Created:', result);
+}
+
